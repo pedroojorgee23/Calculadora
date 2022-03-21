9dd4e173200833fc54d9f8eb4b387b83702c0e2f
pedroojorgee23 <aluno22839@ipt.pt>
2022-03-21 17:13:16 +0000

    Adição do processamento dos algarismos, no Controller

--- Calculadora/Controllers/HomeController.cs
+++ Calculadora/Controllers/HomeController.cs
@@ -13,8 +13,59 @@ namespace Calculadora.Controllers
             _logger = logger;
         }
 
+        [HttpGet] //esta anotação não seria necearia, porque por predefinição os pedidos  HTTP são GET
         public IActionResult Index()
         {
+
+            //inicializar os dados para a calculadora funcionar
+            ViewBag.Visor = "0";
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string botao, string visor)
+        {
+            //vamos decidir o que fazer com o valor do 'botão'
+            switch (botao) {
+                case "1": 
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    //o utilizador pressionou um algarismo
+
+                    if (visor == "0") { visor = botao; }
+                    else { visor = visor + botao; }
+
+
+                    break;
+
+                case ",":
+                    //foi pressionada a ','
+                    if (!visor.Contains(',')) visor += botao;
+
+                    break;
+
+                case "+/-":
+                    //vamos 'inverter' o valor do visor
+                    //pode ser através de uma expressão algébrica
+                    //ou por manipulação de strings
+                    if (visor.StartsWith('-')) visor = visor.Substring(1);
+                    else visor = "-" + visor;
+
+                    break;
+            }
+
+            //preparar os dados a serem enviados para a View
+            ViewBag.Visor = visor;
+
+
             return View();
         }
 
